808b41b20a9313aa45d1f96ee3f60df16e695a67
Corentin JACQUES <jacorentin@gmail.com>
2023-03-13 00:13:21 +0100

    Basic explorer

---
+++ src/components/Tree/View.vue
@@ -0,0 +1,18 @@
+<template>
+  <template v-for="({ id, children }) in tree.children" :key="id">
+    <input :id="'view-' + id" v-bind="children.size ? { type: 'checkbox' } : { type: 'radio', name: 'explorer', value: id }">
+    <label :for="'view-' + id">{{ id }}</label>
+    <div v-if="children.size" class="dir_wrapper">
+      <TreeView :tree="children" />
+    </div>
+  </template>
+</template>
+
+<script>
+export default {
+  name: 'TreeView',
+  props: {
+    tree: Object
+  }
+}
+</script>
